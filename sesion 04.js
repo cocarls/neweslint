d3a7beb6864b06d0e11f7e5dc620ea2be29eba82
cocarls <carloscoronadouribe@gmail.com>
2023-02-22 10:28:35 -0500

    mieslint

---
+++ sesion 04.js
@@ -0,0 +1,5 @@
+const nombre = "Juan"; 
+const edad = 30;
+ const mensaje1 = "Hola, mi nombre es " + nombre + " y tengo " + edad + " años."; 
+const mensaje2 = `Hola, mi nombre es ${nombre} y tengo ${edad} años.`;
+ const mensaje3 = "Hola, mi nombre es " + nombre + " y tengo " + edad + " años."; 
